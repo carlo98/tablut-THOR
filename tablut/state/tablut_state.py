6fec9d37c468aaa1cb5d58b8e043e801164ab0b8
Zamp97 <zamprogno.giacomo@gmail.com>
2020-11-08 18:21:44 +0100

    Added json string to state functionality.

--- tablut/state/tablut_state.py
+++ tablut/state/tablut_state.py
@@ -1,20 +1,35 @@
-"""
-
-"""
+import numpy as np
 
 
-class StateExchanger:
-    """
+class State:
 
-    """
     def __init__(self):
-        """
-
-        """
         pass
 
-    def load_state_from_json(self, server_string, color):
-        """
+    def __init__(self, json_string):
+        white_bitboard= np.zeros(9, dtype=int)
+        black_bitboard = np.zeros(9, dtype=int)
+        king_bitboard = np.zeros(9, dtype=int)
 
-        """
+        turn = json_string["turn"]
+        i_row = 0
+        for row in json_string["board"]:
+            for col in row:
+                if col == "WHITE":
+                    white_bitboard[i_row] ^= 1
+                elif col == "BLACK":
+                    black_bitboard[i_row] ^= 1
+                elif col == "KING":
+                    king_bitboard[i_row] ^= 1
+            white_bitboard[i_row] <<= 1
+            black_bitboard[i_row] <<= 1
+            king_bitboard[i_row] <<= 1
+        i_row +=1
         pass
+
+    def __init__(self, turn, start_pos, end_pos):
+        pass
+
+    def compute_heuristic(game):
+        total_value = 0
+        return total_value
