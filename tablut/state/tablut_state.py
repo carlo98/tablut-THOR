e8eb169ba1ba5e17a3f385684a9387a06eb58f57
Zamp97 <zamprogno.giacomo@gmail.com>
2020-11-09 20:51:32 +0100

    Added capture logic

--- tablut/state/tablut_state.py
+++ tablut/state/tablut_state.py
@@ -7,23 +7,23 @@ from tablut.utils.bitboards import *
 class State:
 
     def __init__(self, json_string):
-        white_bitboard = np.zeros(9, dtype=int)
-        black_bitboard = np.zeros(9, dtype=int)
-        king_bitboard = np.zeros(9, dtype=int)
+        self.white_bitboard = np.zeros(9, dtype=int)
+        self.black_bitboard = np.zeros(9, dtype=int)
+        self.king_bitboard = np.zeros(9, dtype=int)
 
-        turn = json_string["turn"]
+        self.turn = json_string["turn"]
         i_row = 0
         for row in json_string["board"]:
             for col in row:
                 if col == "WHITE":
-                    white_bitboard[i_row] ^= 1
+                    self.white_bitboard[i_row] ^= 1
                 elif col == "BLACK":
-                    black_bitboard[i_row] ^= 1
+                    self.black_bitboard[i_row] ^= 1
                 elif col == "KING":
-                    king_bitboard[i_row] ^= 1
-            white_bitboard[i_row] <<= 1
-            black_bitboard[i_row] <<= 1
-            king_bitboard[i_row] <<= 1
+                    self.king_bitboard[i_row] ^= 1
+            self.white_bitboard[i_row] <<= 1
+            self.black_bitboard[i_row] <<= 1
+            self.king_bitboard[i_row] <<= 1
         i_row += 1
         pass
 
