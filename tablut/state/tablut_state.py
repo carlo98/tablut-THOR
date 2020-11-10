dc1e4e5ff453d55232743cc8ad56d0c644a2aa08
Zamp97 <zamprogno.giacomo@gmail.com>
2020-11-10 16:06:01 +0100

    First draft of heuristic function

--- tablut/state/tablut_state.py
+++ tablut/state/tablut_state.py
@@ -44,31 +44,62 @@ class State:
             else:
                 self.king_bitboard[start_row] -= (1 << (8 - start_col))
                 self.king_bitboard[end_row] += (1 << (8 - end_col))
-
+            self.black_bitboard = white_tries_capture_black_pawn(self.white_bitboard + self.king_bitboard,
+                                                                 self.black_bitboard, end_row, end_col)
         else:
             self.turn = "WHITE"
             self.black_bitboard[start_row] -= (1 << (8 - start_col))
             self.black_bitboard[end_row] += (1 << (8 - end_col))
-
+            self.white_bitboard = black_tries_capture_white_pawn(self.black_bitboard, self.white_bitboard, end_row,
+                                                                 end_col)
+            self.king_bitboard = black_tries_capture_king(self.black_bitboard, self.king_bitboard, end_row, end_col)
         pass
 
-    def compute_heuristic(self,weights):
-        h=0
+    def check_victory(self):
+        if np.count_nonzero(self.king_biboard) == 0:
+            "king captured"
+            return -1
+        if np.count_nonzero(self.king_bitboard & escapes_bitboard) != 0:
+            "king escaped"
+            return 1
+        return 0
+
+    def compute_heuristic(self, weights):
+        "victory condition, of course"
+        victory_cond = weights[0] * self.check_victory()
+
+        "if the exits are blocked, white has a strong disadvantage"
+        blocks_occupied_by_black = np.count_nonzero(self.black_bitboard & blocks_bitboard)
+        blocks_occupied_by_white = np.count_nonzero(self.white_bitboard & blocks_bitboard) + \
+                                   np.count_nonzero(self.king_bitboard & blocks_bitboard)
+        blocks_cond = -weights[1] * blocks_occupied_by_black + weights[2] * blocks_occupied_by_white
+
+        "remaining pieces are considered"
+        white_cnt = 0
+        black_cnt = 0
+        for r in range(0, 9):
+            for c in range(0, 9):
+                curr_mask = 1 << (8 - c)
+                if self.white_bitboard[r] & curr_mask != 0:
+                    white_cnt += 1
+                if self.black_bitboard[r] & curr_mask != 0:
+                    black_cnt += 1
+
+        remaining_whites_cond = weights[3] * (8 - white_cnt)
+        remaining_blacks_cond = weights[4] * (16 - black_cnt)
+
+        h = victory_cond + blocks_cond + remaining_whites_cond + remaining_blacks_cond
         return h
 
     def get_hash(self):
         """
         Returns an identifier for the state.
-        Identifier is not unique, probing required in eventual hash table.
+        Identifier is not unique, probing required in eventual has table.
         """
-        king_sum = 0
-        white_sum = 0
-        black_sum = 0
-        for row in range(len(self.black_bitboard)):
-            king_sum += self.king_bitboard[row]*(row+1)
-            white_sum += self.white_bitboard[row]*(row+1)
-            black_sum += self.black_bitboard[row]*(row+1)
-        return king_sum, white_sum, black_sum
+        state_id = 0
+        for row in range(len(self.white_bitboard)):
+            state_id += row * (self.white_bitboard[row] + self.black_bitboard[row] + self.king_bitboard[row])
+        return state_id
 
     def equal(self, m_bitboards):
         """
@@ -85,4 +116,3 @@ class State:
             if self.king_bitboard[row] ^ m_bitboards["king"] != 0:
                 return False
         return True
-
