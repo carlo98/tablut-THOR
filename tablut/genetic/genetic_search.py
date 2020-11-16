b5c4b181f09165fd7ef2460fb775480ea9508d8a
Carlo Cena <carlo.cena@protonmail.com>
2020-11-16 20:39:50 +0100

    Added genetic_search that uses dataset

--- tablut/genetic/genetic_search.py
+++ tablut/genetic/genetic_search.py
@@ -1,132 +1,163 @@
 """
-Date: 05/11/2020
-Author: Carlo Cena & Giacomo Zamprogno
+Date: 16/11/2020
+Author: Carlo Cena
 
-Population-based search for best weight of heuristic's component.
+Population-based search for best weight of heuristic's component, using data obtained
+by random matches.
 """
-from tablut.client.tablut_client import Client
-from multiprocessing import Pool
-from tablut.utils.state_utils import q
-import subprocess
 import numpy as np
-import time
+import os
+import pickle
+from tablut.utils.state_utils import *
+from tablut.utils.bitboards import *
+from multiprocessing import Process, Array
 
 N_POP = 200  # Number of solutions in population.
-NUM_MATCH = 1  # Percentage of solutions to play against.
 N_PARAM = 6  # Number of parameter of each solution
 MAX_PARAM_VALUE = 40  # Maximum value allowed for each parameter
 MIN_PARAM_VALUE = 0  # Minimum value allowed for each parameter
-MAX_ITER = 200  # Maximum number of iterations
+MAX_ITER = 1000  # Maximum number of iterations
 PERC_NEW_POP = .3  # Percentage of new individuals at each iteration
 EPS = MAX_PARAM_VALUE / 5  # Maximum change of each parameter due to mutation
 MAX_ITER_NO_BETTER = 10  # Maximum number of iterations without better solution
-MAX_TIME_ACTION = 1  # Maximum time allowed to search for action
+N_PROC = 4  # Keep N_POP/N_PROC integer
 
 
-def create_play_player(port, color, max_time_act, weights, name, host, args=None):
-    client_m = Client(port, color, max_time_act, weights=weights, name=name, host=host)
-    client_m.run(args)
+def check_victory(state):
+    if np.count_nonzero(state['king_bitboard']) == 0:
+        "king captured"
+        return -1
+    if np.count_nonzero(state['king_bitboard'] & escapes_bitboard) != 0:
+        "king escaped"
+        return 1
+    return 0
 
 
-def eval_match(sol1, sol2):
-    """
-    Starts two matches between a couple of solutions,
-    returns point earned by each one.
-    """
-    sol1_points = 0
-    sol2_points = 0
-    result = []
-    pool = Pool()
-    proc = subprocess.Popen(['ant', 'server'])  # Requires server files in path
-    time.sleep(2)
-    white_thread = pool.apply_async(create_play_player,
-                                    args=(5800, "WHITE", MAX_TIME_ACTION, sol1, "sol1", "127.0.0.1", result))
-    # Important, pass list just to one of the two threads, to avoid synchronization problems
-    black_thread = pool.apply_async(create_play_player,
-                                    args=(5801, "BLACK", MAX_TIME_ACTION, sol2, "sol2", "127.0.0.1", result))
-    black_thread.wait()
-    white_thread.wait()
-    pool.close()
-    while not q.empty():
-        result.append(q.get())
-    proc.wait()
-    proc.terminate()
+def open_king_paths(state):
+    "king coordinates"
+    king_row = np.nonzero(state['king_bitboard'])[0]
+    king_bin_col = state['king_bitboard'][king_row]
+    king_col = int(8 - np.log2(king_bin_col))
 
-    if result[0] == "WHITE":
-        sol1_points += 3
-    elif result[0] == "BLACK":
-        sol2_points += 3
+    "check for pawns/camps left and right"
+    right_mask = 511 * np.ones(1, dtype=int)
+
+    left_mask = (king_bin_col << 1) * np.ones(1, dtype=int)
+    for col in range(0, king_col+1):
+        right_mask >>= 1
+        if col <= king_col - 2:
+            left_mask ^= king_bin_col
+            left_mask <<= 1
+
+    "check for pawns/camps up and down"
+    above_the_column = []
+    below_the_column = []
+    for row in range(0, 9):
+        if row != king_row and row < king_row:
+            above_the_column += list(bit(camps_bitboard[row])) + list(bit(state['white_bitboard'][row])) + \
+                                list(bit(state['black_bitboard'][row]))
+        elif row != king_row and row > king_row:
+            below_the_column += list(bit(camps_bitboard[row])) + list(bit(state['white_bitboard'][row])) + \
+                                list(bit(state['black_bitboard'][row]))
+    open_paths = 4
+
+    if (king_row in [3, 4, 5] or
+            ((right_mask & state['white_bitboard'][king_row]) + (right_mask & state['black_bitboard'][king_row])
+            + (right_mask & camps_bitboard[king_row]) != 0)):
+        open_paths -= 1
+    if (king_row in [3, 4, 5] or
+            (left_mask & state['white_bitboard'][king_row]) + (left_mask & state['black_bitboard'][king_row])
+            + (left_mask & camps_bitboard[king_row]) != 0):
+        open_paths -= 1
+
+    if king_col in [3, 4, 5] or king_bin_col in above_the_column:
+        open_paths -= 1
+    if king_col in [3, 4, 5] or king_bin_col in below_the_column:
+        open_paths -= 1
+
+    return open_paths
+
+
+def compute_heuristic(s, color, weights):
+    state = {'king_bitboard': np.array(s[0], np.int), 'white_bitboard': np.array(s[1], np.int),
+             'black_bitboard': np.array(s[2], np.int)}
+    "victory condition, of course"
+    victory_cond = check_victory(state)
+    if victory_cond == -1 and color == "BLACK":  # king captured and black player -> Win
+        return -1
+    elif victory_cond == -1 and color == "WHITE":  # King captured and white player -> Lose
+        return 1
+    elif victory_cond == 1 and color == "BLACK":  # King escaped and black player -> Lose
+        return 1
+    elif victory_cond == 1 and color == "WHITE":  # King escaped and white player -> Win
+        return -1
+
+    "if the exits are blocked, white has a strong disadvantage"
+    blocks_occupied_by_black = np.count_nonzero(state['black_bitboard'] & blocks_bitboard)
+    blocks_occupied_by_white = np.count_nonzero(state['white_bitboard'] & blocks_bitboard) + \
+                               np.count_nonzero(state['king_bitboard'] & blocks_bitboard)
+    coeff_min_black = (-1) ** (color == "BLACK")
+    coeff_min_white = (-1) ** (color == "WHITE")
+    blocks_cond = coeff_min_black * weights[0] * blocks_occupied_by_black \
+                    + coeff_min_white * weights[1] * blocks_occupied_by_white
+    open_blocks_cond = coeff_min_white * weights[2] * (8 - blocks_occupied_by_white - blocks_occupied_by_black)
+    "remaining pieces are considered"
+    white_cnt = 0
+    black_cnt = 0
+    for r in range(0, 9):
+        for c in range(0, 9):
+            curr_mask = 1 << (8 - c)
+            if state['white_bitboard'][r] & curr_mask != 0:
+                white_cnt += 1
+            if state['black_bitboard'][r] & curr_mask != 0:
+                black_cnt += 1
+
+    remaining_whites_cond = coeff_min_white * weights[3] * white_cnt
+    remaining_blacks_cond = coeff_min_black * weights[4] * black_cnt
+
+    "aggressive king condition"
+    ak_cond = coeff_min_white * weights[5] * open_king_paths(state)
+    return_value = blocks_cond + remaining_whites_cond + remaining_blacks_cond + open_blocks_cond + ak_cond
+    if return_value > 0:
+        return_value = 0
     else:
-        sol1_points += 1
-        sol2_points += 1
-
-    result = []
-    pool = Pool()
-    proc = subprocess.Popen(['ant', 'server'])  # Requires server files in path
-    time.sleep(2)
-    white_thread = pool.apply_async(create_play_player,
-                                    args=(5800, "WHITE", MAX_TIME_ACTION, sol2, "sol2", "127.0.0.1", result))
-    # Important, pass list just to one of the two threads, to avoid synchronization problems
-    black_thread = pool.apply_async(create_play_player,
-                                    args=(5801, "BLACK", MAX_TIME_ACTION, sol1, "sol1", "127.0.0.1", result))
-    black_thread.wait()
-    white_thread.wait()
-    while not q.empty():
-        result.append(q.get())
-    pool.close()
-    proc.wait()
-    proc.terminate()
-
-    if result[0] == "WHITE":
-        sol2_points += 3
-    elif result[0] == "BLACK":
-        sol1_points += 3
-    else:
-        sol2_points += 1
-        sol1_points += 1
-
-    return sol1_points, sol2_points
+        return_value /= -MAX_PARAM_VALUE
+    return return_value
 
 
-def eval_pop(solutions):
+def eval_pop_thread(solutions, m_state_hash_table, prob_surv, id_m):
     """
     Evaluates solutions, returns a list of floats, between 0 and 1
     (probabilities of survival and reproduction).
     """
-    prob_surv = np.zeros(N_POP, dtype=np.float)
-    num_games = np.zeros(N_POP, dtype=np.int)
     np.random.shuffle(solutions)
-    for index_sol1 in range(len(solutions)):
-        old_index = index_sol1
-        for match_num in range(NUM_MATCH):
-            index_sol2 = np.random.randint(old_index, N_POP)
-            while index_sol2 == old_index:
-                index_sol2 = np.random.randint(0, N_POP)
-            point_sol1, point_sol2 = eval_match(solutions[index_sol1], solutions[index_sol2])
-            prob_surv[index_sol1] += point_sol1
-            prob_surv[index_sol2] += point_sol2
-            num_games[index_sol1] += 2
-            num_games[index_sol2] += 2
-            old_index = index_sol2
+    step = int(len(solutions)/N_PROC)
+    for index_sol in range(len(solutions)):
+        sol = solutions[index_sol]
+        print("Solution ", index_sol, " Id: ", id_m)
+        for state_key in m_state_hash_table:
+            state = m_state_hash_table[state_key]
+            prob_surv[index_sol+step*id_m] += np.sqrt((compute_heuristic(state_key, 'white', sol) -
+                                                       state['value']['white']/state['games'])**2 +
+                                                      (compute_heuristic(state_key, 'black', sol) -
+                                                       state['value']['black']/state['games'])**2 +
+                                                      0.00000000001)
+
+
+def eval_pop(solutions, m_state_hash_table):
+    prob_surv = Array('d', range(N_POP))
+    proc_list = []
+    step = int(len(solutions)/N_PROC)
+    for i_m in range(N_PROC):
+        proc_list.append(Process(target=eval_pop_thread, args=(solutions[N_PROC*i_m: N_PROC*i_m+step],
+                                                               m_state_hash_table, prob_surv, i_m)))
+        proc_list[i_m].start()
+    for i_m in range(N_PROC):
+        proc_list[i_m].join()
     prob_surv = np.array(prob_surv)
-    prob_surv /= 3*num_games
+    prob_surv /= len(state_hash_table)
+    print(prob_surv)
     return prob_surv
-
-
-def test_eval(solutions):
-    max_dist = []
-    for sol in solutions:
-        par_first = 0
-        par_second = 0
-        for u in range(0, int(N_PARAM/2)):
-            par_first += sol[u]
-        for u in range(int(N_PARAM/2), N_PARAM):
-            par_second += sol[u]
-        max_dist.append(np.abs(float(par_first-par_second)) if par_first >= par_second else 0.0)
-
-    max_dist = np.array(max_dist)
-    max_dist /= 4000
-    return max_dist
 
 
 def mate(sol1, sol2):
@@ -181,25 +212,17 @@ def mutations(solutions, prob_surv):
                         solutions[j][param_index] = MIN_PARAM_VALUE
 
 
-def find_best_sol(solutions):
-    """
-    Finding best solution, by evaluating all possible couples.
-    """
-    points = np.zeros(N_POP, dtype=np.int)
-    for index_sol1 in range(len(solutions)):
-        for index_sol2 in range(len(solutions)):
-            if index_sol2 > index_sol1:
-                point_sol1, point_sol2 = eval_match(solutions[index_sol1], solutions[index_sol2])
-                points[index_sol1] += point_sol1
-                points[index_sol2] += point_sol2
-    return np.argmax(points)
-
+exists = os.path.isfile('state_hash')
+if exists:
+    file = open("state_hash", "rb")
+    state_hash_table = pickle.load(file)
+    file.close()
 
 num_iter = 0
 population = []
 for i in range(N_POP):  # Randomly initializing population
     population.append([np.random.randint(MIN_PARAM_VALUE, MAX_PARAM_VALUE+1) for x in range(N_PARAM)])
-prob_survival = eval_pop(population)  # First evaluation of pop
+prob_survival = eval_pop(population, state_hash_table)  # First evaluation of pop
 best_sol = []
 best_sol_prob = 0.0
 no_best_sol = 0
@@ -216,7 +239,7 @@ while num_iter <= MAX_ITER and no_best_sol <= MAX_ITER_NO_BETTER:
         add_newborn(newborn, population, prob_survival)  # Add new individual by removing less strong old ones
         i += 1
     mutations(population, prob_survival)  # Mutations hit less strong individual with higher probability
-    prob_survival = eval_pop(population)  # Population evaluation
+    prob_survival = eval_pop(population, state_hash_table)  # Population evaluation
     best_sol_find_index = int(np.argmax(prob_survival))
     if best_sol_prob < prob_survival[best_sol_find_index]:
         best_sol = population[best_sol_find_index]
@@ -227,6 +250,3 @@ while num_iter <= MAX_ITER and no_best_sol <= MAX_ITER_NO_BETTER:
     print("Iteration: ", num_iter)
     num_iter += 1
     no_best_sol += 1
-
-index_best_sol = find_best_sol(population)
-print("Best sol: ", population[int(index_best_sol)])
