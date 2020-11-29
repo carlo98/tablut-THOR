46c1628d576e5f54c97fbb01ce7e6f41b59958ae
Zamp97 <zamprogno.giacomo@gmail.com>
2020-11-29 15:24:35 +0100

    blackH

--- src/thor/BitStateBlackPlayer.java
+++ src/thor/BitStateBlackPlayer.java
@@ -27,21 +27,37 @@ public class BitStateBlackPlayer extends BitState {
 	@Override
 	public double compute_heuristic() {
 		int blocks_occupied_by_black = 0;
-		int white_cnt = 1, black_cnt = 0;
-        int curr_mask, remaining_whites_cond, remaining_blacks_cond, ak_cond;
-        double blocks_cond;
-
+		int white_cnt = 0, black_cnt = 0;
+        int curr_mask, difference_cond, ak_cond, camps_cond;
+        int blocks_cond;
+		double h_partial, h_return;
+        double threshold = 500;
+        
+        /*look up tables*/
+        int [] camps_lut= {0,-15,-30,-45,-60};
+        int [] blocks_lut = {0, 10, 20, 30, 40, 50, 60, 70, 80};
+        int [] ak_lut = {0, -100, -1000, -1000, -1000};
+        //int [] difference_lut = {-60,-40,-20,-5,0,5,20,40,80};
+        
+        //check for victory position
         int victory_cond = this.check_victory();
         if (victory_cond == -1)  // king captured and black player -> Win
             return Utils.MAX_VAL_HEURISTIC;
         else if (victory_cond == 1)  // King escaped and black player -> Lose
             return -Utils.MAX_VAL_HEURISTIC;
 
+        //diagonal blocks halts the escape for the king, from there, the blockade strategy can be applied
         for(int i = 0; i < this.black_bitboard.length; i++) {
         	blocks_occupied_by_black += Integer.bitCount(this.black_bitboard[i] & Utils.blocks_bitboard[i]);
         }
      
-        blocks_cond = weights[0] * blocks_occupied_by_black;
+        blocks_cond = blocks_occupied_by_black;
+        
+        //backline camps should be opened first, to have more available pawns
+        
+        camps_cond = this.locked_back_camps();
+        
+        //difference in pieces is a good indicator of advantage
         for (int r = 0; r < this.black_bitboard.length; r++) {
             for (int c = 0; c < this.black_bitboard.length; c++) {
                 curr_mask = (1 << (8 - c));
@@ -53,12 +69,20 @@ public class BitStateBlackPlayer extends BitState {
                 }
             }
         }
-
-        remaining_whites_cond = -1 * weights[3] * white_cnt;
-        remaining_blacks_cond = weights[4] * black_cnt;
-
-        ak_cond = -1* weights[5] * this.open_king_paths();
-        return blocks_cond + remaining_whites_cond + remaining_blacks_cond  + ak_cond;
+        difference_cond = (black_cnt-16) - (white_cnt-8);
+        
+        //it is necessary to avoid the king having any number of open paths
+        ak_cond = this.open_king_paths();
+        
+        h_partial = blocks_lut[blocks_cond]  + camps_lut[camps_cond] 
+        		-40*white_cnt + 30*black_cnt + ak_lut[ak_cond];
+        
+        
+		if (h_partial <= threshold) 
+        	h_return = h_partial;
+        else
+        	h_return = h_partial;
+        return h_return;
 	}
 	
 
